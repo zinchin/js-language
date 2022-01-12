132274118bb66208da1baa9d5de5fe8c888036a0
Artem Koroliuk <artemkoroliuk86@gmail.com>
2022-01-12 19:51:07 +0200

    home work 10

---
+++ app.js
@@ -0,0 +1,48 @@
+/*jshint esversion: 6 */
+console.log("hello world");
+//output for var 3 3 3
+/*for (var i=0; i<3; i++){
+    setTimeout(function(){
+        console.log(i);
+    });
+}
+for (let i=0; i<3; i++){
+    setTimeout (function(){
+        console.log(i);
+    });
+}
+const fun=function(a,b){
+    return a+b;
+}
+let a=5;
+console.log(a**2);
+console.log(fun(10,20));
+console.log("12"+12);
+console.log("12"-12);
+console.log(+"12"+12);*/
+const fun=function(a,b){
+    return a+b;
+}
+//home work
+//1.
+console.log("a"+fun**2+"a"+"s");
+//2.
+const calculator=function(a,b,c){
+    switch(c){
+        case "+":
+            return a+b; break;
+        case "-":
+            return a-b; break;
+        case "*":
+            return a*b; break;
+        case "/":
+            return a/b; break;
+    }
+}
+console.log(calculator(100,2,"/"));
+const funy=function(5){function(5,10){
+    
+}}
+
+
+
