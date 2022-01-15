dd78cbb8acb589f675d4e8ed81c1e5c5bccdf137
Artem Koroliuk <artemkoroliuk86@gmail.com>
2022-01-15 11:18:05 +0200

    15.01 HW11

--- app.js
+++ app.js
@@ -1,48 +1,52 @@
 /*jshint esversion: 6 */
-console.log("hello world");
-//output for var 3 3 3
-/*for (var i=0; i<3; i++){
-    setTimeout(function(){
-        console.log(i);
-    });
-}
-for (let i=0; i<3; i++){
-    setTimeout (function(){
-        console.log(i);
-    });
-}
-const fun=function(a,b){
-    return a+b;
-}
-let a=5;
-console.log(a**2);
-console.log(fun(10,20));
-console.log("12"+12);
-console.log("12"-12);
-console.log(+"12"+12);*/
-const fun=function(a,b){
-    return a+b;
-}
-//home work
-//1.
-console.log("a"+fun**2+"a"+"s");
-//2.
-const calculator=function(a,b,c){
-    switch(c){
-        case "+":
-            return a+b; break;
-        case "-":
-            return a-b; break;
-        case "*":
-            return a*b; break;
-        case "/":
-            return a/b; break;
-    }
-}
-console.log(calculator(100,2,"/"));
-const funy=function(5){function(5,10){
-    
-}}
+const str1 = "abc";
+const str2 = "abc";
+console.log(`str1 == str2 is ${str1 == str2}`);
+console.log(`"abc" < "ab" is ${str1 < "ab"}`);
+console.log(`"123" > 23 is ${"123" > 23}`);
+console.log(`"123" > "23" is ${"123" > "23"}`);
+/****************************************** */
+//if  logical expression contains both string and number, JS will convert string to number
+//if string doesn't contain a number a result of conversion will be NaN
+//In any logical expression if there is NaN, then the result will be false
+console.log(`"abc" > 23 is ${"abc" > 23}`);
+console.log(`"abc" < 23 is ${"abc" < 23}`);
+console.log(`"abc" != 23 is ${!("abc" == 23)}`)
+/********************************************* */
+function stringProcessing(str) {
+        const strP = "" + str;
+       console.log(`"${str}"[4] is ${str[4]}`);
+       console.log(`length of "${str}" is ${strP.length}`);
+       console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
+       console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
+       console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of substring "ll" in the "${str}" is  ${strP.lastIndexOf("ll")}`)
 
+}
+//stringProcessing("abcd*lmn*ll(oo&&ttll");
+/********************HW 11 definition */
+// function encode(num, base) {
+//         //base from 2 to 10
+//         let res="";
+//         do {
+//                 const digit = Math.trunc(num % base);
+//                 const symb = getSymbol(digit);
+//                 res = symb + res;
+//                 num = Math.trunc(num / base);
 
-
+//         } while(num >= 1);
+//         return res;
+// }
+// function getSymbol(digit) {
+//       //base from 2 to 10 
+//       console.log(digit);
+//       return "" + digit; //it will work only for base <= 10 
+// }
+console.log(encode(10, 2))
+// write function 
+function encode (num, codingString) {
+        //codingString - any string with no repeated symbols
+        // you should validate codingString doesn't contain repeated symbols
+        //base = length of codingString
+        // algorithm the same as specified above
+        // getSymbol(digit, codingString) using operator []
+}
