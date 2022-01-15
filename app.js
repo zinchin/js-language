71249e0d75787df9a370a2431883fd6dbbcb3269
Artem Koroliuk <artemkoroliuk86@gmail.com>
2022-01-15 20:28:28 +0200

    home work 11

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ console.log(`"123" > "23" is ${"123" > "23"}`);
 //In any logical expression if there is NaN, then the result will be false
 console.log(`"abc" > 23 is ${"abc" > 23}`);
 console.log(`"abc" < 23 is ${"abc" < 23}`);
-console.log(`"abc" != 23 is ${!("abc" == 23)}`)
+console.log(`"abc" != 23 is ${!("abc" == 23)}`);
 /********************************************* */
 function stringProcessing(str) {
         const strP = "" + str;
@@ -19,7 +19,7 @@ function stringProcessing(str) {
        console.log(`length of "${str}" is ${strP.length}`);
        console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
        console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
-       console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of substring "ll" in the "${str}" is  ${strP.lastIndexOf("ll")}`)
+       console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of substring "ll" in the "${str}" is  ${strP.lastIndexOf("ll")}`);
 
 }
 //stringProcessing("abcd*lmn*ll(oo&&ttll");
@@ -41,12 +41,42 @@ function stringProcessing(str) {
 //       console.log(digit);
 //       return "" + digit; //it will work only for base <= 10 
 // }
-console.log(encode(10, 2))
+//console.log(encode(10, 2));
 // write function 
-function encode (num, codingString) {
+//function encode (num, codingString) {
         //codingString - any string with no repeated symbols
         // you should validate codingString doesn't contain repeated symbols
         //base = length of codingString
         // algorithm the same as specified above
         // getSymbol(digit, codingString) using operator []
+        
+function encode (num,codingString ){
+        let res="";
+        if (checkCodingString()==false)
+        console.log("error");
+        else
+        do{
+        const base=codingString.length;
+        const digit=Math.trunc(num%base);
+        const codingStringI=codingString[digit];
+        const symb=getSymbol(codingStringI);
+        res=symb+res;
+        num=Math.trunc(num/base);
+        }while(num>=1);
+           return res;
 }
+function getSymbol(codingStringI){
+        //console.log(digit);
+        return ""+codingStringI;
+}
+function checkCodingString(codingString){
+  for(i=0;i<codingString.length;i++){
+      for(j=i;j<codingString.length;j++)
+       { console.log(j);
+if(codingString[i]===codingString[j]) return false;
+
+else return true;
+      }
+}      
+}
+console.log(encode(3,["s","d","s","n"]));
